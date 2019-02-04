d0d752c3fc1379a5aef5e0eb08fded27428e3522
Elijah <ejoebanks@gmail.com>
2019-02-04 10:05:13 -0600

    Added profile view

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,6 +30,7 @@
     </head>
     <body>
       <div id="app">
+        
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
           <div class="container">
             <a class="navbar-brand" href="/">

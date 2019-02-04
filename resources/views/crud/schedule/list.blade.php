43c4ec23f3b4d005f6ae00bd5da04574042c1045
Elijah <ejoebanks@gmail.com>
2019-02-04 10:05:01 -0600

    Added table search

--- resources/views/crud/schedule/list.blade.php
+++ resources/views/crud/schedule/list.blade.php
@@ -4,19 +4,27 @@
 <?php
 if (Auth::user() != null && Auth::user()->type == 1) {
     ?>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-3">
+        <input class="form-control" id="system-search" name="q" placeholder="Search" required>
+        <br/>
+      </div>
+    </div>
+  </div>
 
 <div class="container">
   <div class="row">
+    <br/>
     <?php
     foreach (range('A', 'D') as $char) {
         ?>
     <div class="col-sm">
       <h1> Shift {{ $char }} </h1>
-      <table class="table table-striped">
+      <table class="table table-list-search table table-striped">
           <thead>
               <tr>
                 <td>ID</td>
-                <td>Shift</td>
                 <td>Name</td>
               </tr>
           </thead>
@@ -25,7 +33,6 @@ if (Auth::user() != null && Auth::user()->type == 1) {
               @if($char == $s->shift)
               <tr>
                   <td>{{$s->id}}</td>
-                  <td>{{$s->shift}}</td>
                   <td>{{$s->firstName}} {{ $s->lastName }}</td>
               </tr>
               @endif

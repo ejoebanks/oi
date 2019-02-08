3fff746ec659d25314b540390d03d36d171a22f9
Elijah <ejoebanks@gmail.com>
2019-02-07 20:44:58 -0600

    Dropdown addition, styling

--- resources/views/crud/schedule/list.blade.php
+++ resources/views/crud/schedule/list.blade.php
@@ -24,7 +24,7 @@ if (Auth::user() != null && Auth::user()->type == 1) {
     <div id="sCount">{{ $shiftcount }}</div>
     <br/>
 
-      <h1> Shift {{ $char }} </h1>
+      <h1 id="shift"> Shift {{ $char }} </h1>
       <table class="table table-list-search table table-striped">
           <thead>
               <tr>
@@ -36,7 +36,18 @@ if (Auth::user() != null && Auth::user()->type == 1) {
               @foreach($schedule as $s)
               @if($char == $s->shift)
               <tr>
-                  <td>{{$s->id}}</td>
+                <td><div class="btn-group">
+                  <button class="btn btn-secondary btn-sm dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    {{$s->id}}
+                  </button>
+                  <div class="dropdown-menu">
+                    <a class="dropdown-item" href="">Shift</a>
+                    <div class="dropdown-divider"></div>
+                    @include('functions.shiftdropdown')
+                  </div>
+                </div>
+                </td>
+              </td>
                   <td>{{$s->firstName}} {{ $s->lastName }}</td>
               </tr>
               @endif

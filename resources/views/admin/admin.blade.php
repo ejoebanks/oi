f677c6bad8dde270171456f01b1360345326dd10
Elijah <ejoebanks@gmail.com>
2019-02-13 17:51:53 -0600

    Added button in CRUD for events

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -10,6 +10,7 @@
       <div class="col-sm-4">
           <a href="/schedule" button type="button" class="btn btn-primary btn-lg btn-block">Schedule</button></a>
           <a href="/users" button type="button" class="btn btn-primary btn-lg btn-block">Staff</button></a>
+          <a href="/events" button type="button" class="btn btn-primary btn-lg btn-block">Events</button></a>
       </div>
 
       <div class="col-sm-4">

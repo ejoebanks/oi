0fdabaae7d398e449c7bb88c310526e4b6e2d89e
Elijah <ejoebanks@gmail.com>
2019-02-17 15:29:22 -0600

    Changed button style, page bg color

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -8,9 +8,9 @@
         </div>
 
       <div class="col-sm-4">
-          <a href="/schedule" button type="button" class="btn btn-primary btn-lg btn-block">Schedule</button></a>
-          <a href="/users" button type="button" class="btn btn-primary btn-lg btn-block">Staff</button></a>
-          <a href="/events" button type="button" class="btn btn-primary btn-lg btn-block">Events</button></a>
+          <a href="/schedule" button type="button" class="btn btn-secondary btn-lg btn-block">Schedule</a>
+          <a href="/users" button type="button" class="btn btn-secondary btn-lg btn-block">Staff</a>
+          <a href="/events" button type="button" class="btn btn-secondary btn-lg btn-block">Events</a>
       </div>
 
       <div class="col-sm-4">

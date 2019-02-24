7812f1c062986639b6996b6b2e7916d54108101b
Elijah <ejoebanks@gmail.com>
2019-02-24 16:36:37 -0600

    Change wording of button due to new table (staff)

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -10,7 +10,7 @@
       <div class="col-sm-4">
           <a href="/staff" button type="button" class="btn btn-secondary btn-lg btn-block">Staff</a>
           <a href="/schedule" button type="button" class="btn btn-secondary btn-lg btn-block">Schedule</a>
-          <a href="/users" button type="button" class="btn btn-secondary btn-lg btn-block">Staff</a>
+          <a href="/users" button type="button" class="btn btn-secondary btn-lg btn-block">Users</a>
           <a href="/events" button type="button" class="btn btn-secondary btn-lg btn-block">Events</a>
 
       </div>

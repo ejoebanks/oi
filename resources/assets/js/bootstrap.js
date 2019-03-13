2bed8eec246b672dcef466c602d187ed69ca12a2
Elijah <ejoebanks@gmail.com>
2019-03-13 17:51:22 -0500

    Added jquery req

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -1,3 +1,9 @@
+try {
+  window.$ = window.jQuery = require('jquery');
+  window.Popper = require('popper.js').default;
+
+  require('bootstrap');
+} catch (e) {}
 
 window._ = require('lodash');
 window.Popper = require('popper.js').default;

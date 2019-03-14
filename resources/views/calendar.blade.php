4bfadfab1b9acca3b8b0ac4a4c5f23b913cc519c
Elijah <ejoebanks@gmail.com>
2019-03-14 18:30:42 -0500

    Calendar tweaks

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -4,7 +4,32 @@
 
 <div class="container">
     <div class="row">
-        <div class="col-sm-offset-1 col-sm-10"><div id='calendar'></div>
+        <div class="col-sm-offset-1 col-sm-10">
+        </div>
+        <div id='calendar'></div>
+
+        <script>
+    $(document).ready(function() {
+        $('#calendar').fullCalendar({
+            events : [
+                @foreach($event as $ev)
+                {
+                    title : '{{ $ev->title }}',
+                    start : '{{ $ev->date }}',
+                    url : '{{ route('events.edit', $ev->id) }}'
+                },
+                @endforeach
+            ],
+            header: {
+              left: 'prev,next today myCustomButton',
+              center: 'title',
+              right: 'month,list'
+            }
+
+        })
+    });
+</script>
+
 </div>
     </div>
 </div>

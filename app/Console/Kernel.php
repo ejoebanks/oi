1e15aa622af14ac8a7b93138e5e9edfa4ca51d5b
Elijah <ejoebanks@gmail.com>
2019-03-26 20:29:31 -0500

    Added notification for EventReminder

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,7 +7,10 @@ use DB;
 use Carbon;
 use App\ShiftChange;
 use App\Order;
+use App\Event;
+use App\User;
 use App\Notifications\ChangeNotification;
+use App\Notifications\EventReminder;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
@@ -20,22 +23,39 @@ class Kernel extends ConsoleKernel
     {
         $schedule->call(function () {
             $shiftchanges = ShiftChange::
-                            whereBetween('created_at',
-                            [Carbon::now()->startOfWeek(),
-                            Carbon::now()->endOfWeek()])
+                            whereBetween(
+                                'created_at',
+                                [Carbon::now()->startOfWeek(),
+                            Carbon::now()->endOfWeek()]
+                            )
                             ->where('notified', '!=', 1)
                             ->get();
 
             foreach ($shiftchanges as $change) {
-                    $sendTo = \App\User::find($change->clockNumber);
-                    $sendTo->notify(new ChangeNotification());
+                $sendTo = \App\User::find($change->clockNumber);
+                $sendTo->notify(new ChangeNotification());
 
-                    // Update so e-mail doesn't get sent repeatedly
-                    $thisChange = ShiftChange::find($change->id);
-                    //$thisChange->notified = 1;
-                    $thisChange->save();
+                // Update so e-mail doesn't get sent repeatedly
+                $thisChange = ShiftChange::find($change->id);
+                //$thisChange->notified = 1;
+                $thisChange->save();
             }
-          });
+
+            $events = Event::
+                            whereBetween(
+                                'date',
+                                [Carbon::now()->startOfWeek(),
+                            Carbon::now()->endOfWeek()]
+                            )
+                            ->get();
+            $users = User::all();
+
+            foreach ($events as $event) {
+                foreach ($users as $user) {
+                    $user->notify(new EventReminder());
+                }
+            }
+        });
     }
 
     protected function commands()

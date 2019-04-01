4384d790e418c29617bf2344325ea4c4f5d6e1d4
Elijah <ejoebanks@gmail.com>
2019-04-01 12:34:54 -0500

    Moved rows, headings so table is exported properly

--- resources/views/orgchart/chart.blade.php
+++ resources/views/orgchart/chart.blade.php
@@ -6,38 +6,38 @@
   <table class="table table-list-search table table-striped">
       <thead>
           <tr>
-            <td>
-              {{$char}}
-						</td>
+            <th class="shiftheader" colspan="2">Shift {{$char}}</th>
           </tr>
       </thead>
       <tbody>
         @php ($stack = array())
         @foreach ($shifts as $shift)
             @if ($char == $shift->shift)
-            <tr>
             @php (array_push($stack, $char))
               @if ($stack[0] !== $char)
                 @php ($stack = array())
                 @php (array_push($stack, $char))
               @endif
               @if (in_array("$shift->primaryJob", $stack))
-                <td><strong>{{$shift->clockNumber}}</strong>: {{$shift->firstName}} {{$shift->lastName}}</td>
+              <tr>
+                <td><strong>{{$shift->clockNumber}}</strong></td>
+                <td>{{$shift->firstName}} {{$shift->lastName}}</td>
+              </tr>
               @else
-              <thead>
-								<tr>
-                  <td class="jobheader">
-                    {{ $shift->primaryJob }}
-                  </td>
+              <tr>
+                <th class="jobheader" colspan="2">
+                   {{ $shift->primaryJob }}
+                </th>
+              </tr>
+                <tr>
+                  <td><strong>{{$shift->clockNumber}}</strong></td>
+                  <td>{{$shift->firstName}} {{$shift->lastName}}</td>
                 </tr>
-              </thead>
-                <td><strong>{{$shift->clockNumber}}</strong>: {{$shift->firstName}} {{$shift->lastName}}</td>
               @endif
               @if (in_array("$shift->primaryJob", $stack))
               @else
                 @php (array_push($stack, $shift->primaryJob))
               @endif
-            </tr>
             @endif
           @endforeach
 				</tbody>

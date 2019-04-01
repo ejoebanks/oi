f821db492fcd1250ae06fc67bf3c06a4e057f04f
Elijah <ejoebanks@gmail.com>
2019-04-01 10:14:15 -0500

    Added export to download from database

---
+++ app/Exports/ShiftsFromView.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Exports;
+
+use App\Shift;
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithEvents;
+use Maatwebsite\Excel\Events\AfterSheet;
+
+class ShiftsFromView implements FromCollection, WithHeadings, ShouldAutoSize, WithEvents
+{
+    public function collection()
+    {
+
+        $shifts = Shift::join('staff', 'staff.clockNumber', '=', 'shifts.clockNumber')
+              ->select('shifts.clockNumber', 'staff.firstName', 'staff.lastName', 'shifts.shift', 'shifts.primaryJob')
+              ->orderBy('shift', 'ASC')
+              ->orderBy('primaryJob', 'ASC')
+              ->get();
+
+        return $shifts;
+    }
+
+    public function headings(): array
+    {
+        return [
+        '#',
+        'First Name',
+        'Last Name',
+        'Shift',
+        'Primary Job'
+    ];
+    }
+
+    public function registerEvents(): array
+{
+    return [
+        AfterSheet::class=> function(AfterSheet $event) {
+            $cellRange = 'A1:W1'; // All headers
+            $event->sheet->getDelegate()->getStyle($cellRange)->getFont()->setSize(14);
+        },
+    ];
+}
+
+}

38c7280c4b64401b9405e87d51a6829f545c0158
Elijah <ejoebanks@gmail.com>
2019-04-05 20:57:24 -0500

    Added styling to spreadsheet headers

--- app/Exports/ShiftInfoFromView.php
+++ app/Exports/ShiftInfoFromView.php
@@ -4,8 +4,10 @@ use App\Shift;
 use Illuminate\Contracts\View\View;
 use Maatwebsite\Excel\Concerns\FromView;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithEvents;
+use Maatwebsite\Excel\Events\AfterSheet;
 
-class ShiftInfoFromView implements FromView, ShouldAutoSize
+class ShiftInfoFromView implements FromView, ShouldAutoSize, WithEvents
 {
     public function view(): View
     {
@@ -18,4 +20,15 @@ class ShiftInfoFromView implements FromView, ShouldAutoSize
 
         ]);
     }
+
+    public function registerEvents(): array
+{
+    return [
+        AfterSheet::class=> function(AfterSheet $event) {
+            $cellRange = 'A1:W1'; // All headers
+            $event->sheet->getDelegate()->getStyle($cellRange)->getFont()->setSize(14);
+        },
+    ];
+}
+
 }

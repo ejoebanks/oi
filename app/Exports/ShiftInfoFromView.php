4fdb25dc8f791c87e2a9eddda9d47d19c0637f5a
Elijah <ejoebanks@gmail.com>
2019-04-07 18:20:08 -0500

    Added border to bottom of header, added diagram of cells to botom

--- app/Exports/ShiftInfoFromView.php
+++ app/Exports/ShiftInfoFromView.php
@@ -51,7 +51,32 @@ class ShiftInfoFromView implements FromView, ShouldAutoSize, WithEvents
                         'argb' => '173CA0',
                     ],
                 ],
+                'borders' => [
+                    'bottom' => [
+                        'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THICK,
+                    ],
+                ],
+
             ];
+
+            $footerStyle = [
+                  'font' => [
+                      'size' => 16,
+                      'bold' => true,
+                      'color' => array('rgb' => 'ffffff'),
+                  ],
+                  'alignment' => [
+                      'horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER,
+                  ],
+                  'borders' => [
+                        'outline' => [
+                            'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THICK,
+                            'color' => ['argb' => '140000'],
+                        ],
+                  ],
+
+              ];
+
             $shifts = Shift::join('staff', 'staff.clockNumber', '=', 'shifts.clockNumber')
                 ->select('shifts.clockNumber', 'staff.firstName', 'staff.lastName', 'shifts.shift', 'shifts.primaryJob')
                 ->get();
@@ -60,25 +85,6 @@ class ShiftInfoFromView implements FromView, ShouldAutoSize, WithEvents
 
             $grouped->toArray();
 
-/*
-            $arr = array();
-            $cellVal = array();
-            $cellVal[0] = "A";
-            $i = 0;
-            foreach($shifts as $shift){
-              if ($i >= 23){
-                $i = 0;
-              }
-              $i++;
-              if ($cellVal[0] !== $shift->shift){
-                $cellVal[0] = $shift->shift;
-              }
-              if (!in_array($shift->primaryJob, $cellVal) && $cellVal[0] == $shift->shift){
-                array_push($arr, $shift->primaryJob);
-                array_push($cellVal, $shift->shift.$i);
-              }
-            }
-            */
             foreach(range('A', 'D') as $char){
               $pos = 1;
              foreach($grouped["$char"] as $group){
@@ -107,7 +113,6 @@ class ShiftInfoFromView implements FromView, ShouldAutoSize, WithEvents
                     ->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)
                     ->getStartColor()->setARGB('b3a78f');
               }
-
 
               if ($group->primaryJob == "G0609"){
                 $event->sheet->getStyle($cell)->getFill()
@@ -138,19 +143,40 @@ class ShiftInfoFromView implements FromView, ShouldAutoSize, WithEvents
                     ->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)
                     ->getStartColor()->setARGB('698b80');
               }
-
-
-
-
-
-
             }
           }
 
+            $event->sheet->setCellValue('A30', 'G0809')->getStyle('A30')->getFill()->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)
+            ->getStartColor()->setARGB('698b80');
+
+            $event->sheet->setCellValue('B30', 'G1003')->getStyle('B30')->getFill()->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)
+            ->getStartColor()->setARGB('85c1cc');
+
+            $event->sheet->setCellValue('C30', 'G0609')->getStyle('C30')->getFill()->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)
+            ->getStartColor()->setARGB('a1bfdc');
+
+            $event->sheet->setCellValue('D30', 'G0410')->getStyle('D30')->getFill()->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)
+            ->getStartColor()->setARGB('b3a78f');
+
+            $event->sheet->setCellValue('A31', 'GD640')->getStyle('A31')->getFill()->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)
+            ->getStartColor()->setARGB('eeea88');
+
+            $event->sheet->setCellValue('B31', 'G4010')->getStyle('B31')->getFill()->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)
+            ->getStartColor()->setARGB('7a93b0');
+
+            $event->sheet->setCellValue('C31', 'G0807')->getStyle('C31')->getFill()->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)
+            ->getStartColor()->setARGB('8cabe1');
+
+            $event->sheet->setCellValue('D31', 'G410')->getStyle('D31')->getFill()->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)
+            ->getStartColor()->setARGB('ff555d');
 
             $cellRange = 'A1:D1';
             $event->sheet->setCellValue('A1', 'Shift A');
             $event->sheet->getStyle($cellRange)->applyFromArray($styleArray);
+            $event->sheet->getStyle('A30:D30')->applyFromArray($footerStyle);
+            $event->sheet->getStyle('A31:D31')->applyFromArray($footerStyle);
+    );
+
         },
     ];
 }

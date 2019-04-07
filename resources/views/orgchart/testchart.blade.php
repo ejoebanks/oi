39e522ea9112336948008653676a24df016cc0dc
Elijah <ejoebanks@gmail.com>
2019-04-06 22:52:20 -0500

    Fixed errant ifs, decrements in wrong places

--- resources/views/orgchart/testchart.blade.php
+++ resources/views/orgchart/testchart.blade.php
@@ -19,53 +19,54 @@
         </thead>
         <tbody>
           			 @php ($stack = array())
-  							 @php ($a = count($grouped['A']) - 1)
+  							 @php ($a = count($grouped['A']))
+                 <pre>
   							 @php ($b = count($grouped['B']) - 1)
   							 @php ($c = count($grouped['C']) - 1)
   							 @php ($d = count($grouped['D']) - 1)
 
   							 @foreach(range('A', 'D') as $char)
   							 	@foreach($grouped["$char"] as $group)
-  								@if ($a < 0 && $b < 0 && $c < 0 && $d < 0)
+  								@if ($a <= 0 && $b <= 0 && $c <= 0 && $d <= 0)
   								 <?php break; ?>
   								@endif
   								      <tr>
   												<td>
-  													@if ($a > 0)
-                              {{$grouped['A'][$a]->clockNumber}}
-  														{{$grouped['A'][$a]->firstName}}
-  														{{$grouped['A'][$a]->lastName}}
+  													@if ($a >= 0)
+                              {{$group->clockNumber}}
+  														{{$group->firstName}}
+  														{{$group->lastName}}
+                              @php($a--)
   													@endif
   												</td>
 
   												<td>
-  													@if ($b > 0)
+  													@if ($b >= 0)
                               {{$grouped['B'][$b]->clockNumber}}
   														{{$grouped['B'][$b]->firstName}}
                               {{$grouped['B'][$b]->lastName}}
+                              @php($b--)
   													@endif
   												</td>
 
   												<td>
-  													@if ($c > 0)
+  													@if ($c >= 0)
   														{{$grouped['C'][$c]->clockNumber}}
                               {{$grouped['C'][$c]->firstName}}
                               {{$grouped['C'][$c]->lastName}}
+                              @php($c--)
   													@endif
   												</td>
 
   												<td>
-  													@if ($d > 0)
+  													@if ($d >= 0)
   														{{$grouped['D'][$d]->clockNumber}}
                               {{$grouped['D'][$d]->firstName}}
                               {{$grouped['D'][$d]->lastName}}
+                              @php($d--)
   													@endif
   												</td>
   								       </tr>
-  											 @php($a--)
-  											 @php($b--)
-  											 @php($c--)
-  											 @php($d--)
   							@endforeach
   						@endforeach
   				</tbody>

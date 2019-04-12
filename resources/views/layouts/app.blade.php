9e7bee66ab5bbc73fb57caac05c2608d42be3825
Elijah <ejoebanks@gmail.com>
2019-04-11 20:52:53 -0500

    Hid navbar links if not auth, org chart if not admin

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -48,7 +48,7 @@
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <i class="fas fa-chevron-circle-down"></i>
             </button>
-
+            @auth
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
               <ul class="navbar-nav mr-auto">
                 <li class="nav-item">
@@ -57,13 +57,15 @@
                     Events
                   </a>
                 </li>
-
+                @if ($id == "1")
                 <li class="nav-item">
                   <a class="nav-link" href="/orgchart">
                     <i class="fas fa-chart-bar"></i>
                     Org Chart
                   </a>
                 </li>
+                @endif
+            @endauth
               </ul>
                 <ul class="navbar-nav ml-auto">
                   <!-- Authentication Links -->

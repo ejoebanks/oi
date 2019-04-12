d2a21e9dd0914a3880f802ab973530ae481cfdc7
Elijah <ejoebanks@gmail.com>
2019-04-12 15:58:24 -0500

    Commented out getAttribute that was throwing error

--- public/js/custom.js
+++ public/js/custom.js
@@ -26,45 +26,44 @@ $(document).ready(function() {
     } else {
       $("#primaryJob").prop('required',true);
       $("#shift").prop('required',true);
-
     }
-
   });
 
-    const allEqual = arr => arr.every( v => v === arr[0] );
-    var shiftA = document.getElementById('shiftA').getAttribute('value');
-    var shiftB = document.getElementById('shiftB').getAttribute('value');
-    var shiftC = document.getElementById('shiftC').getAttribute('value');
-    var shiftD = document.getElementById('shiftD').getAttribute('value');
-    var min = Math.min(shiftA, shiftB, shiftC, shiftD);
 
-    var alphabet = "ABCD".split("");
-    _.each(alphabet, function(letter) {
-      if (document.getElementById('shift'+letter).getAttribute('value') == min && !allEqual([shiftA, shiftB, shiftC, shiftD])){
+//    const allEqual = arr => arr.every( v => v === arr[0] );
+//    var shiftA = document.getElementById('shiftA').getAttribute('value');
+//    var shiftB = document.getElementById('shiftB').getAttribute('value');
+//    var shiftC = document.getElementById('shiftC').getAttribute('value');
+//    var shiftD = document.getElementById('shiftD').getAttribute('value');
+//    var min = Math.min(shiftA, shiftB, shiftC, shiftD);
+
+//    var alphabet = "ABCD".split("");
+//    _.each(alphabet, function(letter) {
+//      if (document.getElementById('shift'+letter).getAttribute('value') == min && !allEqual([shiftA, shiftB, shiftC, shiftD])){
         //document.getElementById('shift'+letter).style.backgroundColor = "#CCCC00";
-      } else {
+//      } else {
         //document.getElementById('shift'+letter).style.backgroundColor = "#4D8C57";
-      }
-    });
+//      }
+//    });
 
     var activeSystemClass = $('.list-group-item.active');
 
     //Something is entered into the input
     $('#system-search').keyup( function() {
-       var that = this;
+        var that = this;
         // Look through all table rows
         var tableBody = $('.table-list-search tbody');
         var tableRowsClass = $('.table-list-search tbody tr');
+        var collapseClass = $('.collapse-search');
+
         $('.search-sf').remove();
         tableRowsClass.each( function(i, val) {
-
             //Avoid case sensitivity
             var rowText = $(val).text().toLowerCase();
             var inputText = $(that).val().toLowerCase();
             if(inputText != '')
             {
                 $('.search-query-sf').remove();
-
                 //tableBody.prepend('<tr class="search-query-sf"><td colspan="6"><strong>Searching for: "'
                 //    + $(that).val()
                 //    + '"</strong></td></tr>');
@@ -78,13 +77,14 @@ $(document).ready(function() {
             {
                 //Hide rows
                 tableRowsClass.eq(i).hide();
-
+                collapseClass.eq(i).hide();
             }
             else
             {
                 $('.search-sf').remove();
                 tableRowsClass.eq(i).show();
             }
+
         });
         //Hide all TR elements if nothing found
         if(tableRowsClass.children(':visible').length == 0)
@@ -93,9 +93,48 @@ $(document).ready(function() {
         }
     });
 });
+$(document).ready(function(){
 
-$.ajaxSetup({
-     headers: {
-           'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-       }
-   });
+  $.ajaxSetup({
+    headers: {
+        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+    }
+});
+
+  $("#search").keyup(function(){
+      var searchText = $(this).val().toLowerCase();
+      // Show only matching TR, hide rest of them
+      $.each($("#table tbody tr"), function() {
+          if($(this).text().toLowerCase().indexOf(searchText) === -1)
+             $(this).hide();
+          else
+             $(this).show();
+      });
+  });
+
+
+  (function(){
+  var searchTerm, panelContainerId;
+  // Create a new contains that is case insensitive
+  $.expr[':'].containsCaseInsensitive = function (n, i, m) {
+    return jQuery(n).text().toUpperCase().indexOf(m[3].toUpperCase()) >= 0;
+  };
+
+  $('#accordion_search_bar').on('change keyup paste click', function () {
+    searchTerm = $(this).val();
+    $('#accordion > .panel').each(function () {
+      panelContainerId = '#' + $(this).attr('id');
+      $(panelContainerId + ':not(:containsCaseInsensitive(' + searchTerm + '))').hide();
+      $(panelContainerId + ':containsCaseInsensitive(' + searchTerm + ')').show();
+    });
+  });
+}());
+  $("#myInput").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#myTable tr").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+      $(this).$("#EMP44").collapse('show');
+    });
+
+  });
+});

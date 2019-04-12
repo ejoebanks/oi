89e62f49e4fe834db3aaf8479a457bd035d9b4a4
Elijah <ejoebanks@gmail.com>
2019-04-11 20:51:55 -0500

    Removed unused fields, added id

--- app/User.php
+++ app/User.php
@@ -15,29 +15,31 @@ class User extends Authenticatable
     }
 
     protected $fillable = [
-        'id', 'firstName', 'lastName', 'email', 'clockNumber', 'password', 'type'
+        'id', 'firstName', 'lastName', 'email', 'clockNumber', 'password', 'type', 'emergencycontact'
     ];
 
     public function updateUser($data)
     {
         $user = $this->find($data['id']);
+        $staff = Staff::find($data['id']);
         $user->id = $data['id'];
-        $user->firstName = $data['firstName'];
-        $user->lastName = $data['lastName'];
-        $user->emergencycontact = $data['emergencycontact'];
+        //$user->firstName = $data['firstName'];
+        //$user->lastName = $data['lastName'];
+        $staff->emergencycontact = $data['emergencycontact'];
         $user->email = $data['email'];
         $user->type = $data['type'];
         $user->password = bcrypt($data['password']);
         $user->save();
+        $staff->save();
         return 1;
     }
 
     public function singleUpdate($data)
     {
         $user = $this->find($data['id']);
-        $staff = Staff::find($data['id']);
-        $staff->firstName = $data['firstName'];
-        $staff->lastName = $data['lastName'];
+        $staff = Staff::find($data['clockNumber']);
+        //$staff->firstName = $data['firstName'];
+        //$staff->lastName = $data['lastName'];
         $staff->emergencycontact = $data['emergencycontact'];
         $user->email = $data['email'];
         $user->password = bcrypt($data['password']);

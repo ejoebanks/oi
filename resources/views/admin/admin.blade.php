6cc875c826ff54319c07eaa8c1dfa01091230388
Elijah <ejoebanks@gmail.com>
2019-04-13 21:02:00 -0500

    Changed layout of admin page

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -2,22 +2,65 @@
 
 @section('content')
 <br/>
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-sm-4">
-        </div>
-
-      <div class="col-sm-4">
-          <a href="/staff" button type="button" class="btn btn-secondary btn-lg btn-block">Staff</a>
-          <a href="/shifts" button type="button" class="btn btn-secondary btn-lg btn-block">Shifts</a>
-          <a href="/users" button type="button" class="btn btn-secondary btn-lg btn-block">Users</a>
-          <a href="/events" button type="button" class="btn btn-secondary btn-lg btn-block">Events</a>
-          <a href="/shiftchanges" button type="button" class="btn btn-secondary btn-lg btn-block">Shift Changes</a>
+<div class="container-fluid">
+   <div class="row">
+      <div class="col-md-3">
       </div>
-
-      <div class="col-sm-4">
+      <div class="col-md-3">
+         <a href="/users" id="admin_cards">
+            <div class="card-counter staff-admin">
+               <i class="fas fa-users-cog"></i>
+               <span class="count-numbers">{{ $staffcount }}</span>
+               <span class="count-name">Staff</span>
+            </div>
+         </a>
       </div>
-
+      <div class="col-md-3">
+         <a href="/users" id="admin_cards">
+            <div class="card-counter user-admin">
+               <i class="fas fa-users"></i>
+               <span class="count-numbers">{{ $usercount }}</span>
+               <span class="count-name">Users</span>
+            </div>
+         </a>
+      </div>
+      <div class="col-md-3">
+      </div>
+   </div>
+   <div class="row">
+      <div class="col-md-3">
+      </div>
+      <div class="col-md-3">
+         <a href="/shiftchanges" id="admin_cards">
+            <div class="card-counter change-admin">
+               <i class="fas fa-exchange-alt"></i>
+               <span class="count-numbers">{{ $changecount}}</span>
+               <span class="count-name">Shift Changes</span>
+            </div>
+         </a>
+      </div>
+      <div class="col-md-3">
+         <a href="/shifts" id="admin_cards">
+            <div class="card-counter shift-admin">
+               <i class="fas fa-clock"></i>
+               <span class="count-numbers">{{ $shiftcount }}</span>
+               <span class="count-name">Shifts</span>
+            </div>
+         </a>
+      </div>
+      <div class="col-md-3">
+      </div>
+   </div>
+   <div class="row justify-content-center">
+      <div class="col-md-3">
+         <a href="/events" id="admin_cards">
+            <div class="card-counter event-admin">
+               <i class="fas fa-calendar-week"></i>
+               <span class="count-numbers">{{ $eventcount }}</span>
+               <span class="count-name">Events</span>
+            </div>
+         </a>
+      </div>
+   </div>
 </div>
-
 @endsection

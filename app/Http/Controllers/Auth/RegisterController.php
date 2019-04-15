1178e5d4d65daa8bb6231211370374fa44400375
Elijah <ejoebanks@gmail.com>
2019-04-15 15:41:41 -0500

    Fixed clock number not being capitalized properly

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -30,7 +30,7 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'clocknumber' => "nullable|integer|unique:users",
+            'clockNumber' => "nullable|integer|unique:users",
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed',
         ]);
@@ -39,13 +39,13 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         $user = User::create([
-            'clocknumber' => $data['clocknumber'],
+            'clockNumber' => $data['clockNumber'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
             //'type' => User::DEFAULT_TYPE,
         ]);
 
-        $staff = Staff::find($data['clocknumber']);
+        $staff = Staff::find($data['clockNumber']);
         if($staff != null) {
           $staff->emergencycontact = $data['emergencycontact'];
           $staff->save();

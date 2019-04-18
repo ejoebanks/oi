0e6206fdcf08a7e70c9736c180f464fcde9552b6
Elijah <ejoebanks@gmail.com>
2019-04-18 11:45:36 -0500

    Changed layout of form

--- resources/views/crud/shifts/create.blade.php
+++ resources/views/crud/shifts/create.blade.php
@@ -11,26 +11,29 @@
         </ul>
     </div><br />
 @endif
-<div class="container">
+<div class="row">
+  <div class="col">
+  </div>
+  <div class="col-md-8">
     <h2 id="crud_header">Create Shift</h2>
+    <div id="bleh">
+      <form method="post" action="{{ action('ShiftController@store') }}">
+      <div class="row">
+        <div class="col-md-6">
+          <h5>Employee:</h5>
 
-    <form method="post" action="{{ action('ShiftController@store') }}">
-
-      <div class="form-group">
-      <input type="hidden" value="{{csrf_token()}}" name="_token" />
-      <label for="id">Employee:</label>
-
-      <select class="form-control" name="clockNumber" id="clockNumber" required>
-        <option value="">None</option>
-      @foreach($staff as $mem)
-        <option value="<?=$mem->clockNumber ?>"><?= $mem->firstName. " ".$mem->lastName ?></option>
-      @endforeach
-      </select>
-    </div>
-      <div class="form-group">
-          <input type="hidden" value="{{csrf_token()}}" name="_token" />
-          <label for="shift">Shift:</label>
+          <select class="form-control" name="clockNumber" id="clockNumber" required>
+            <option value="">None</option>
+          @foreach($staff as $mem)
+            <option value="<?=$mem->clockNumber ?>"><?= $mem->firstName. " ".$mem->lastName ?></option>
+          @endforeach
+          </select>
+        </div>
+        <br/>
+        <div class="col-md-2">
+          <h5>Shift:</h5>
           <select class="form-control" value='' name="shift" id="shift">
+            <option value="">None</option>
           <?php
           foreach (range('A', 'D') as $char) {
           ?>
@@ -38,25 +41,29 @@
             <?php
             }?>
         </select>
+        </div>
+        <div class="col-md-4">
+          <h5>Primary Job:</h5>
+          <input type="text" class="form-control" name="primaryJob" id="primaryJob" />
+        </div>
       </div>
-
-      <div class="form-group">
-          <input type="hidden" value="{{csrf_token()}}" name="_token" />
-          <label for="primaryJob">Primary Job:</label>
-          <input type="text" class="form-control" name="primaryJob" />
+      <br>
+      <div class="row">
+        <div class="col-md-12">
+        <input type="hidden" value="{{csrf_token()}}" name="_token" />
+        <h5>Comments:</h5>
+        <textarea type="textarea" class="form-control" name="comments" /></textarea>
       </div>
-
-      <div class="form-group">
-          <input type="hidden" value="{{csrf_token()}}" name="_token" />
-          <label for="comments">Comments:</label>
-          <textarea type="textarea" class="form-control" name="comments" /></textarea>
-      </div>
-
-
-        <button type="submit" class="btn btn-primary">Create</button>
-        </form>
-
     </div>
+<br/>
+       <button type="submit" class="btn btn-primary">Create</button>
+
+    </form>
+  </div>
+  </div>
+  <div class="col">
+  </div>
+</div>
 </div>
 
 @endsection
--- resources/views/crud/shifts/edit.blade.php
+++ resources/views/crud/shifts/edit.blade.php
@@ -11,63 +11,70 @@
         </ul>
     </div><br />
 @endif
-    <div class="container">
+<div class="row">
+  <div class="col">
+  </div>
+  <div class="col-md-8">
       <h2 id="crud_header">Edit Shift</h2>
+      <div id="bleh">
       <form class="form-horizontal" role="form" method="POST" action="{{ action('ShiftController@update',$shift->id) }}">
            {!! csrf_field() !!}
-
            <input type="hidden" name="_method" value="PATCH">
 
-           <div class="form-group">
-               <input type="hidden" value="{{csrf_token()}}" name="_token" />
-               <label for="id">Employee:</label>
+           <div class="row">
+             <div class="col-md-6">
+               <h5>Employee:</h5>
 
-               <select class="form-control" name="employee" id="employee" required>
+               <select class="form-control" name="clockNumber" id="clockNumber" required>
                  <option value="">None</option>
-               @foreach($staff as $mem)
-                  @if ($mem->clockNumber == $shift->clockNumber)
-                    @php ($selected = "selected")
-                  @else
-                    @php ($selected = "")
-                  @endif
-                 <option {{ $selected }} value="<?=$mem->clockNumber ?>"><?= $mem->firstName. " ".$mem->lastName ?></option>
-               @endforeach
+                 @foreach($staff as $mem)
+                    @if ($mem->clockNumber == $shift->clockNumber)
+                      @php ($selected = "selected")
+                    @else
+                      @php ($selected = "")
+                    @endif
+                   <option {{ $selected }} value="<?=$mem->clockNumber ?>"><?= $mem->firstName. " ".$mem->lastName ?></option>
+                 @endforeach
                </select>
+             </div>
+             <br/>
+             <div class="col-md-2">
+               <h5>Shift:</h5>
+               <select class="form-control" value='{{ $shift->shift }}' name="shift" id="shift">
+
+               <?php
+               foreach (range('A', 'D') as $char) {
+                 if ($shift->shift == $char) {
+                     $selected = "selected";
+                 } else {
+                     $selected = "";
+                 } ?>
+                 <option {{ $selected }} value="<?= $char ?>"><?=  $char ?></option>
+                 <?php
+                 }?>
+             </select>
+             </div>
+             <div class="col-md-4">
+               <h5>Primary Job:</h5>
+               <input type="text" class="form-control" name="primaryJob" value="{{ $shift->primaryJob }}" />
+             </div>
            </div>
-
-        <div class="form-group">
-            <input type="hidden" value="{{csrf_token()}}" name="_token" />
-            <label for="shift">Shift:</label>
-              <select class="form-control" value='{{ $shift->shift }}' name="shift" id="shift">
-
-              <?php
-              foreach (range('A', 'D') as $char) {
-                if ($shift->shift == $char) {
-                    $selected = "selected";
-                } else {
-                    $selected = "";
-                } ?>
-                <option {{ $selected }} value="<?= $char ?>"><?=  $char ?></option>
-                <?php
-                }?>
-            </select>
-          </div>
-
-            <div class="form-group">
-                <input type="hidden" value="{{csrf_token()}}" name="_token" />
-                <label for="primaryJob">Primary Job:</label>
-                <input type="text" class="form-control" name="primaryJob" value="{{ $shift->primaryJob }}" />
-            </div>
-
-            <div class="form-group">
-                <input type="hidden" value="{{csrf_token()}}" name="_token" />
-                <label for="comments">Comments:</label>
-                <textarea type="textarea" class="form-control" name="comments" />{{ $shift->comments }}</textarea>
-            </div>
+           <br>
+           <div class="row">
+             <div class="col-md-12">
+             <input type="hidden" value="{{csrf_token()}}" name="_token" />
+             <h5>Comments:</h5>
+             <textarea type="textarea" class="form-control" name="comments" />{{ $shift->comments }}</textarea>
+           </div>
         </div>
+        <br/>
 
         <button type="submit" class="btn btn-primary">Update</button>
-        </form>
+      </form>
     </div>
-</div>
+    </div>
+    <div class="col">
+    </div>
+  </div>
+  </div>
 @endsection

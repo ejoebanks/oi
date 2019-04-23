bc14c7f63c5f1a7e38479fbb82fa12cc3d467b1f
Elijah <ejoebanks@gmail.com>
2019-04-22 21:02:35 -0500

    Made table responsive so it doesn't push past container on mobile

--- resources/views/recent.blade.php
+++ resources/views/recent.blade.php
@@ -7,13 +7,13 @@
   <hr class="crud_hr"/>
 
   <div class="row">
-    <div class="col">
+    <div class="col table-responsive">
       <table class="table table-striped" id="recentTab">
-        <thead>
+        <thead class="crud_head">
     <tr>
       <th scope="col">Clock #</th>
       <th scope="col">Name</th>
-      <th scope="col">Current Shift</th>
+      <th scope="col">New Shift</th>
       <th scope="col">Moved From</th>
       <th scope="col">Updated At</th>
     </tr>
@@ -22,11 +22,12 @@
     <?php $i = 0; ?>
       @foreach($recentChanges as $change)
         <tr>
-          <td><h5>{{ $change->id }}</h5></td>
-          <td><h5>{{ $change->firstName }} {{ $change->lastName }}</h5></th>
+          <td><h5>{{ $i }}</h5></td>
+          <td><h5>{{ $change->firstName }} {{ "Smith" }}</h5></th>
           <td><h5>{{ $change->currentshift }}</h5></td>
           <td><h5>{{ $change->prevshift }}</h5></td>
           <td><h5>{{ date('m-d-Y', strtotime($change->created)) }}</h5></td>
+          <?php $i++ ?>
         </tr>
       @endforeach
     </tbody>

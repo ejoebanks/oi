5b52ed13763046ec4d10ae2a6541b1a29981b92e
Elijah <ejoebanks@gmail.com>
2019-04-22 21:01:45 -0500

    Added custom box to password reset pages

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -4,10 +4,12 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div id="login_header" class="card-header">{{ __('Reset Password') }}</div>
+          <div id="login_box">
+                <div class="card-header"></div>
+                <h2 id="crud_header">{{ __('Reset Password') }}</h2>
+                <hr class="orange_hr"/>
 
-                <div class="card-body" id="login_body">
+                <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success">
                             {{ session('status') }}
@@ -18,9 +20,10 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
+                            <div class="col-md-6 offset-md-3">
+                              <div class="float-left">
+                                <h5>Email Address</h5>
+                              </div>
                                 <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
 
                                 @if ($errors->has('email'))
@@ -31,12 +34,10 @@
                             </div>
                         </div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
+                        <div class="form-group row justify-content-center">
                                 <button type="submit" class="btn btn-secondary">
                                     {{ __('Send Password Reset Link') }}
                                 </button>
-                            </div>
                         </div>
                     </form>
                 </div>

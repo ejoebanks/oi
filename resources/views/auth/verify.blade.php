a8dddb4520525680882e51fad7717a195a6263ba
Elijah <ejoebanks@gmail.com>
2019-04-23 19:36:31 -0500

    Added styling to verification page

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -4,19 +4,19 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header custom-card-header">{{ __('Verify Your Email Address') }}</div>
-
-                <div class="card-body custom-card">
+          <div id="login_box">
+                <h2 id="crud_header">{{ __('Verify Your Email Address') }}</h2>
+                <hr class="orange_hr"/>
+                <div class="verify">
                     @if (session('resent'))
                         <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
+                            {{ __('A new verification link has been sent to your email address.') }}
                         </div>
                     @endif
 
                     {{ __('In order to access this website, you need to verify your email address.  Check your inbox for the verification link.') }}
                     <br/></br/>
-                    {{ __('If you did not receive the email') }}, <a id="cardLink" href="{{ route('verification.resend') }}">{{ __('click here to request another') }}</a>.
+                    {{ __('If you did not receive the email') }}, <a id="verifylink" href="{{ route('verification.resend') }}">{{ __('click here to request another') }}</a>.
                 </div>
             </div>
         </div>

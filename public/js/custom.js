079d4855dcd494ff9eb4d94071826d16330ad992
Elijah <ejoebanks@gmail.com>
2019-04-25 10:38:06 -0500

    Added js to handle file upload input

--- public/js/custom.js
+++ public/js/custom.js
@@ -94,6 +94,11 @@ $(document).ready(function() {
     });
 });
 $(document).ready(function(){
+  $('#file').change(function() {
+    var i = $(this).prev('label').clone();
+    var file = $('#file')[0].files[0].name;
+    $(this).prev('label').text(file);
+  });
 
   $.ajaxSetup({
     headers: {

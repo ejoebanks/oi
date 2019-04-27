a54084c469ae98bd1fbdbcbb7046de9839e17f58
Elijah <ejoebanks@gmail.com>
2019-04-26 20:03:25 -0500

    Added update if staff/shift is found, create new if not

--- app/Imports/StaffImport.php
+++ app/Imports/StaffImport.php
@@ -7,7 +7,6 @@ use App\Shift;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
 
-
 class StaffImport implements ToModel
 {
     /**
@@ -17,11 +16,37 @@ class StaffImport implements ToModel
     */
     public function model(array $row)
     {
-      return new Staff([
-         'clockNumber'     => $row[0],
-         'seniority'    => $row[1],
-         'firstName'    => $row[2],
-         'lastName'    => $row[3]
-      ]);
+        $member = Staff::where('clockNumber', $row[0])->first();
+        $shift = Shift::where('clockNumber', $row[0])->first();
+
+        if (!Staff::find($row[0])) {
+            return new Staff([
+               'clockNumber'     => $row[0],
+               'seniority'    => $row[1],
+               'firstName'    => $row[2],
+               'lastName'    => $row[3]
+                ]);
+        } else {
+            $member->update([
+              'seniority' => $row[1],
+              'firstName' => $row[2],
+              'lastName' => $row[3]
+                ]);
+        }
+        if (!$shift) {
+            return new Shift([
+               'clockNumber'   => $row[0],
+               'shift'         => $row[4],
+               'primaryJob'    => $row[5],
+               'comments'      => $row[6]
+                ]);
+        } else {
+            $shift->update([
+               'shift'         => $row[4],
+               'primaryJob'    => $row[5],
+               'comments'      => $row[6]
+                ]);
+        }
+
     }
 }

28148c9f983262a885a5be972ac86cbc4b6da825
Elijah <ejoebanks@gmail.com>
2019-05-02 20:39:52 -0500

    Added button to email recent change spreadsheet

--- resources/views/recent.blade.php
+++ resources/views/recent.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
 <div class="container">
+  <a href="{{action('ShiftChangeController@sendRecent')}}" class="btn btn-outline-info" role="button"><i class="far fa-envelope"></i> Email</a>
   <div id="crud_box">
   <h2 id="crud_header">Recent Changes</h2>
   <hr class="crud_hr"/>
@@ -13,6 +14,7 @@
     <tr>
       <th scope="col">Clock #</th>
       <th scope="col">Name</th>
+      <th scope="col">Job</th>
       <th scope="col">New Shift</th>
       <th scope="col">Moved From</th>
       <th scope="col">Updated At</th>
@@ -23,6 +25,7 @@
         <tr>
           <td><h5>{{ $change->id }}</h5></td>
           <td><h5>{{ $change->firstName }} {{ $change->lastName }}</h5></th>
+          <td><h5>{{ $change->job }}</h5></td>
           <td><h5>{{ $change->currentshift }}</h5></td>
           <td><h5>{{ $change->prevshift }}</h5></td>
           <td><h5>{{ date('m-d-Y', strtotime($change->created)) }}</h5></td>

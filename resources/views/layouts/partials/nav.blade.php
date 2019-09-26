b54fe4500c6e69ec286636d1d825b5ec8647a511
ejoebanks <ejoebanks@gmail.com>
2019-09-26 17:54:04 -0500

    Removed id checks on nav

--- resources/views/layouts/partials/nav.blade.php
+++ resources/views/layouts/partials/nav.blade.php
@@ -11,7 +11,6 @@
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <i class="fas fa-chevron-circle-down"></i>
       </button>
-      @auth
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item">
@@ -20,63 +19,30 @@
               Events
             </a>
           </li>
-          <!--
-          @if ($id == "1")
-          <li class="nav-item">
-            <a class="nav-link" href="/orgchart">
-              <i class="fas fa-chart-bar"></i>
-              Org Chart
-            </a>
-          </li>
-          @endif
-        -->
-      @endauth
         </ul>
           <ul class="navbar-nav ml-auto">
-            <!-- Authentication Links -->
             @guest
-
-            <li>
-              <a class="nav-link" href="{{ route('login') }}">
-                <i class="fas fa-sign-in-alt fa-lg"></i>
-                {{ __('Login') }}
-              </a>
-            </li>
-            <li>
-              <a class="nav-link" href="{{ route('register') }}">
-                <i class="fas fa-user-plus fa-lg"></i>
-                {{ __('Register') }}
-              </a>
-            </li>
-            @else
-            @if($id > 0)
             <li class="nav-item">
               <a class="nav-link" href="/lists">
                 <i class="fa fa-list" aria-hidden="true"></i>
                 Shifts
               </a>
             </li>
-            @endif
             <li class="nav-item dropdown">
               <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                 <i class="fa fa-user-o"></i>
                 </i>
                 <i class="fas fa-user"></i>
-                @if (!is_object(Auth::user()->info))
-                {{ Auth::user()->email }}
-                @else
-                {{ Auth::user()->info->firstName." ".Auth::user()->info->lastName }}
-                @endif
+                Account Name
                 <span class="caret">
                 </span>
               </a>
               <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                @php ($uid = Auth::user()->id)
-                <a href="{{action('UserController@singleEdit', $uid)}}" class="dropdown-item">Update Account
+              <!--
+                <a href="" class="dropdown-item">Update Account
                 </a>
-              @if ($id > 0)
+              -->
                 <a href="/admin" class="dropdown-item">Admin</a>
-              @endif
                 <a class="dropdown-item" href="{{ route('logout') }}"
                    onclick="event.preventDefault();
                             document.getElementById('logout-form').submit();">
@@ -87,6 +53,7 @@
                 </form>
               </div>
             </li>
+            @else
             @endguest
           </ul>
       </div>

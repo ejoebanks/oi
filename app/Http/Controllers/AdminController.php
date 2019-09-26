0f3dbf7a424c02601fa5468a3536059f44d0d22a
ejoebanks <ejoebanks@gmail.com>
2019-09-26 17:54:12 -0500

    Removed auth middleware

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -13,10 +13,10 @@ use Carbon\Carbon;
 
 class AdminController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
+    //public function __construct()
+    //{
+    //    $this->middleware('auth');
+    //}
 
     public function index()
     {
